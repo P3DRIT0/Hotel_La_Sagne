2d005bbcd6cd957ebbb40a1c6ee42fdc6b93cae7
Pablo-O9 <78048016+Pablo-O9@users.noreply.github.com>
2021-06-03 17:10:01 +0200

    Update hotel.sql

--- config/hotel.sql
+++ config/hotel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-06-2021 a las 19:09:20
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 7.4.13
+-- Tiempo de generación: 03-06-2021 a las 17:09:20
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `administradores`
+--
+
+CREATE TABLE `administradores` (
+  `id_admin` bigint(20) NOT NULL,
+  `id_usuario` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `administradores`
+--
+
+INSERT INTO `administradores` (`id_admin`, `id_usuario`) VALUES
+(1, 105);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `habitaciones`
 --
 
@@ -37,9 +55,8 @@ CREATE TABLE `habitaciones` (
 --
 
 INSERT INTO `habitaciones` (`id`, `tipo_habitacion`) VALUES
-(68, 'Individual'),
-(69, 'Individual'),
-(70, 'Individual');
+(71, 'Individual'),
+(72, 'Suite');
 
 -- --------------------------------------------------------
 
@@ -59,7 +76,9 @@ CREATE TABLE `habitaciones_reservas` (
 --
 
 INSERT INTO `habitaciones_reservas` (`id`, `num_reserva`, `id_habitacion`, `fecha_disponibilidad`) VALUES
-(87, 101, 68, '2021-06-04 22:00:00');
+(90, 104, 71, '2021-06-26 22:00:00'),
+(91, 105, 72, '2021-06-10 22:00:00'),
+(92, 106, 71, '2021-06-26 22:00:00');
 
 -- --------------------------------------------------------
 
@@ -68,9 +87,17 @@ INSERT INTO `habitaciones_reservas` (`id`, `num_reserva`, `id_habitacion`, `fech
 --
 
 CREATE TABLE `habitacion_servicio` (
-  `id_habitacion` bigint(20) NOT NULL,
+  `tipo_habitacion` varchar(255) NOT NULL,
   `id_servicio` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `habitacion_servicio`
+--
+
+INSERT INTO `habitacion_servicio` (`tipo_habitacion`, `id_servicio`) VALUES
+('Suite', 4),
+('Suite', 7);
 
 -- --------------------------------------------------------
 
@@ -89,9 +116,12 @@ CREATE TABLE `imagenes_habitaciones` (
 --
 
 INSERT INTO `imagenes_habitaciones` (`id`, `id_tipo_habitacion`, `imagen_habitacion`) VALUES
-(119, 111, '../Reservas/Imagenes_habitaciones/556.jpg'),
-(120, 111, '../Reservas/Imagenes_habitaciones/557.jpg'),
-(121, 111, '../Reservas/Imagenes_habitaciones/558.jpg');
+(133, 116, '../Reservas/Imagenes_habitaciones/581.jpg'),
+(134, 116, '../Reservas/Imagenes_habitaciones/582.jpg'),
+(135, 116, '../Reservas/Imagenes_habitaciones/583.jpg'),
+(136, 117, '../Reservas/Imagenes_habitaciones/586.jpg'),
+(137, 117, '../Reservas/Imagenes_habitaciones/587.jpg'),
+(138, 117, '../Reservas/Imagenes_habitaciones/588.jpg');
 
 -- --------------------------------------------------------
 
@@ -112,7 +142,9 @@ CREATE TABLE `reservas` (
 --
 
 INSERT INTO `reservas` (`num_reserva`, `id_usuario`, `fecha_entrada`, `fecha_salida`, `tipo_habitacion`) VALUES
-(101, 105, '2021-06-01 22:00:00', '2021-06-04 22:00:00', 'Individual');
+(104, 105, '2021-06-03 22:00:00', '2021-06-11 22:00:00', 'Individual'),
+(105, 105, '2021-06-02 22:00:00', '2021-06-10 22:00:00', 'Suite'),
+(106, 105, '2021-06-23 22:00:00', '2021-06-26 22:00:00', 'Individual');
 
 -- --------------------------------------------------------
 
@@ -131,7 +163,8 @@ CREATE TABLE `roles` (
 
 INSERT INTO `roles` (`id`, `nombre_rol`) VALUES
 (2, 'Usuario_administrador'),
-(1, 'Usuario_registrado');
+(1, 'Usuario_registrado'),
+(3, 'Usuario_trabajador');
 
 -- --------------------------------------------------------
 
@@ -152,9 +185,11 @@ CREATE TABLE `servicios` (
 --
 
 INSERT INTO `servicios` (`id`, `nombre_servicio`, `precio_servicio`, `descripcion`, `disponibilidad`) VALUES
-(1, 'Internet', '10.00', 'Servicios de conexion ', b'1'),
-(2, 'Internet', '10.00', 'Servicios', b'1'),
-(3, 'Lavanderia', '9999.99', 'dasdqaw', b'1');
+(4, 'Internet', '123.00', 'dawada', b'1'),
+(5, 'Lavanderia', '1313.00', 'dadsada', b'1'),
+(6, 'Basura', '32.00', 'adawad', b'1'),
+(7, 'Piscina', '123.00', 'dadwa', b'1'),
+(8, 'Bar', '2.00', 'qdad', b'1');
 
 -- --------------------------------------------------------
 
@@ -178,7 +213,23 @@ CREATE TABLE `tipo_habitaciones` (
 --
 
 INSERT INTO `tipo_habitaciones` (`id`, `m2`, `ventana`, `tipo_de_habitacion`, `servicio_limpieza`, `internet`, `precio`, `descripcion`) VALUES
-(111, '123.00', b'1', 'Individual', b'1', b'1', '123.00', 'fghdfghfdghfdgh');
+(116, '123.00', b'1', 'Individual', b'1', b'1', '123.00', 'sqdswqsd'),
+(117, '123.00', b'1', 'Suite', b'1', b'1', '123.00', 'qdqasdqdsqad');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `trabajadores`
+--
+
+CREATE TABLE `trabajadores` (
+  `id_trabajadores` bigint(20) NOT NULL,
+  `id_usuario` bigint(20) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `dni` varchar(9) NOT NULL,
+  `nacionalidad` varchar(255) NOT NULL,
+  `sexo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -202,12 +253,19 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `telf`, `direccion`, `password`, `rol_usuario`, `imagen_usuario`) VALUES
-(100, 'pablo', 'pablo@gmail.com', '123456789', 'callex', '$2y$10$PQmzgIIsFElSNu78U.BsSef2ow7SOTuusn5P6h1tHC4X4bLtiK1SC', 2, '../Perfil/Multimedia/7e683d738d1da240fb12f3d8eb86db0e.jpg'),
-(105, 'Pedro', 'pedrofc99@gmail.com', '698139991', 'tuputacasa', '$2y$10$XTRTnaAtpHoi52EBlG/zP.5RV9g2AKBkaQYvV1xOGHB6rUynuVzSu', 1, '../Perfil/Multimedia/avatar_defecto.png');
+(100, 'pablo', 'pablo@gmail.com', '123456789', 'callex', '$2y$10$PQmzgIIsFElSNu78U.BsSef2ow7SOTuusn5P6h1tHC4X4bLtiK1SC', 2, '../Perfil/Multimedia/0786b5b80ccd512c28a839d2b67d1a0d.jpg'),
+(105, 'Pedro', 'pedrofc99@gmail.com', '698139991', 'tuputacasa', '$2y$10$XTRTnaAtpHoi52EBlG/zP.5RV9g2AKBkaQYvV1xOGHB6rUynuVzSu', 2, '../Perfil/Multimedia/0786b5b80ccd512c28a839d2b67d1a0d.jpg');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `administrador_roles_2` (`id_usuario`);
 
 --
 -- Indices de la tabla `habitaciones`
@@ -228,8 +286,9 @@ ALTER TABLE `habitaciones_reservas`
 -- Indices de la tabla `habitacion_servicio`
 --
 ALTER TABLE `habitacion_servicio`
-  ADD PRIMARY KEY (`id_habitacion`,`id_servicio`),
-  ADD KEY `id_servicio` (`id_servicio`);
+  ADD PRIMARY KEY (`tipo_habitacion`,`id_servicio`) USING BTREE,
+  ADD KEY `id_servicio_fk` (`id_servicio`) USING BTREE,
+  ADD KEY `tipo_habitacion_fk` (`tipo_habitacion`) USING BTREE;
 
 --
 -- Indices de la tabla `imagenes_habitaciones`
@@ -267,6 +326,13 @@ ALTER TABLE `tipo_habitaciones`
   ADD UNIQUE KEY `tipo_de_habitacion` (`tipo_de_habitacion`) USING BTREE;
 
 --
+-- Indices de la tabla `trabajadores`
+--
+ALTER TABLE `trabajadores`
+  ADD PRIMARY KEY (`id_trabajadores`),
+  ADD KEY `trabajador_roles_2` (`id_usuario`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -280,40 +346,52 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  MODIFY `id_admin` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `habitaciones`
 --
 ALTER TABLE `habitaciones`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 
 --
 -- AUTO_INCREMENT de la tabla `habitaciones_reservas`
 --
 ALTER TABLE `habitaciones_reservas`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT de la tabla `imagenes_habitaciones`
 --
 ALTER TABLE `imagenes_habitaciones`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142;
 
 --
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `num_reserva` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `num_reserva` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_habitaciones`
 --
 ALTER TABLE `tipo_habitaciones`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+
+--
+-- AUTO_INCREMENT de la tabla `trabajadores`
+--
+ALTER TABLE `trabajadores`
+  MODIFY `id_trabajadores` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -324,6 +402,12 @@ ALTER TABLE `usuarios`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `administradores`
+--
+ALTER TABLE `administradores`
+  ADD CONSTRAINT `administrador_roles_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 
 --
 -- Filtros para la tabla `habitaciones`
@@ -342,7 +426,7 @@ ALTER TABLE `habitaciones_reservas`
 -- Filtros para la tabla `habitacion_servicio`
 --
 ALTER TABLE `habitacion_servicio`
-  ADD CONSTRAINT `habitacion_servicio_ibfk_1` FOREIGN KEY (`id_habitacion`) REFERENCES `tipo_habitaciones` (`id`),
+  ADD CONSTRAINT `habitacion_servicio_ibfk_1` FOREIGN KEY (`tipo_habitacion`) REFERENCES `tipo_habitaciones` (`tipo_de_habitacion`),
   ADD CONSTRAINT `habitacion_servicio_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id`);
 
 --
@@ -356,6 +440,12 @@ ALTER TABLE `imagenes_habitaciones`
 --
 ALTER TABLE `reservas`
   ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `trabajadores`
+--
+ALTER TABLE `trabajadores`
+  ADD CONSTRAINT `trabajador_roles_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
 
 --
 -- Filtros para la tabla `usuarios`
